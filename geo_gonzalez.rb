06dd1f9fb3a9ae5dcad7407c9c80709bcf20fa14
ggonza55 <ggonza55@uic.edu>
2022-09-27 13:33:49 -0500

    Uploaded beginning of Project

---
+++ geo_gonzalez.rb
@@ -0,0 +1,11 @@
+class BST_Set
+  attr_accessor :root, :size
+
+  def initialize()
+    @root = nil
+    @size = 0
+  end
+end
+
+# TODO: Use Tree Nodes
+# reference: https://medium.com/analytics-vidhya/implement-a-binary-search-tree-in-ruby-c3fa9192410b
