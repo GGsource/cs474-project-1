5c883833455f24b689d050290d73bfd97774831a
GGPC <ggonza55@uic.edu>
2022-09-28 14:33:43 -0500

    Implemented TreeSet and TreeNode

--- geo_gonzalez.rb
+++ geo_gonzalez.rb
@@ -5,7 +5,42 @@ class BST_Set
     @root = nil
     @size = 0
   end
-end
 
-# TODO: Use Tree Nodes
-# reference: https://medium.com/analytics-vidhya/implement-a-binary-search-tree-in-ruby-c3fa9192410b
+  def add(givenVal)
+    if @root == nil
+      @root = TreeNode.new(givenVal)
+    else
+      curNode = @root
+      prevNode = @root
+
+      while curNode != nil
+        prevNode = curNode
+        if givenVal == curNode.val #We're making sets so NO REPEATS ALLOWED!!
+          puts "BAD! Cannot put duplicate values in a set!"
+          return
+        elsif givenVal < curNode.val
+          curNode = curNode.leftNode
+        else #else the given value is bigger than currents so it must go on the right
+          curNode = curNode.rightNode
+        end
+      end
+
+      if givenVal < prevNode.val
+        prevNode.leftNode = TreeNode.new(givenVal)
+      else
+        prevNode.rightNode = TreeNode.new(givenVal)
+      end
+    end
+    @size += 1
+  end
+
+  class TreeNode
+    attr_accessor :leftNode, :rightNode, :val
+
+    def initialize(givenVal)
+      @leftNode = nil
+      @rightNode = nil
+      @val = givenVal
+    end
+  end
+end
