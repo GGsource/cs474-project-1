9396cdf71b83cb0bbb9528b20301565b77ee00a8
ggonza55 <ggonza55@uic.edu>
2022-09-29 22:27:07 -0500

    Removed redudant variables & Condensed ifs to case

--- main.rb
+++ main.rb
@@ -10,8 +10,8 @@ require_relative "BST_Set"
 #############################################################################################
 # Helper Functions
 #############################################################################################
-#setVals -
-#Function for setting the given set to the given values. Commands X, Y and Z all use this.
+# setVals -
+# Function for setting the given set to the given values. Commands X, Y and Z all use this.
 def setVals(givenSet, args)
   givenSet.clear #First clear out anything old
   for val in args
@@ -33,26 +33,26 @@ end
 # Main Program Loop
 #############################################################################################
 setX, setY, setZ = BST_Set.new, BST_Set.new, BST_Set.new #Our 3 main sets, beginning as null
-input = nil            #Where we receive the user's raw input
-args = []              #Where we will split the user's input by spaces
-until false
+until false #Infinite Loop until forced to break
+  # Greet the user and request their command
   print "Hello, welcome to CS474 Project 1! Please input your command: "
-  input = gets
-  splitInput = input.split(" ", 2)
-  cmd = splitInput[0]
-  args = splitInput[1].split(",")
-  if cmd == "X" #command for filling Set Y with new values
-    setVals(setX, args)
-  elsif cmd == "Y" #command for filling Set Y with new values
-    setVals(setY, args)
-  elsif cmd == "Z" #command for filling Set Y with new values
-    setVals(setZ, args)
-  elsif cmd == "q" #User wants to quit, exit the loop
+  # TODO: List available commands so the user knows
+  input = gets                  #Get user input
+  input = input.split(" ", 2)   #Split user input into command and arguments
+  cmd = input[0]                #Command the user wants to use
+  args = input[1].split(",")    #Arguments the user is providing for the command
+
+  # Check which command the user chose
+  case cmd
+  when "X".."Z" #command for filling set X, Y, or Z with new values
+    setVals(binding.local_variable_get("set" + cmd), args)
+  when "q" #User wants to quit, exit the loop
     break
   else
     # Give error message to let the user know their mistake. Colored red!
     puts "\e[#{31}m    ERROR:\e[0m #{cmd} is not a valid command! Please Refer to the command list."
   end
+  # Print out the 3 sets at the end of each loop
   puts "Set X: #{setX}\nSet Y: #{setY}\nSet Z: #{setZ}"
 end
 print "Exiting program, goodbye!"
