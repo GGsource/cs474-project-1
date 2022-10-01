1c499cfc6c1b3129601aa958790f352ddc9bdde2
ggonza55 <ggonza55@uic.edu>
2022-09-30 19:39:38 -0500

    Thoroughly commented BST_Set class

--- BST_Set.rb
+++ BST_Set.rb
@@ -1,49 +1,43 @@
-# TODO: Comment all these functions
-class BST_Set
-  attr_accessor :root, :size
-
+class BST_Set #Class for our sets in tree form
   def initialize()
     @root = nil
-    @size = 0
   end
 
-  def add(givenVal)
-    if @root == nil
+  def add(givenVal) #Adds the passed in number to this set, unless its duplicate
+    if @root == nil #if this set is empty the new node goes at the root
       @root = TreeNode.new(givenVal)
     else
-      curNode = @root
-      prevNode = @root
+      curNode = @root #Copy of root to traverse without changing root
+      prevNode = @root #prev set to same as cur for now
 
       while curNode != nil
         prevNode = curNode
         if givenVal == curNode.val #We're making sets so NO REPEATS ALLOWED!!
           puts "\e[#{33}m    Warning:\e[0m Duplicates not allowed. Keeping only single instance of #{givenVal}\n"
           return
-        elsif givenVal < curNode.val
+        elsif givenVal < curNode.val #given value is less than current so it must go on the left
           curNode = curNode.leftNode
-        else #else the given value is bigger than currents so it must go on the right
+        else #given value is bigger than currents so it must go on the right
           curNode = curNode.rightNode
         end
       end
-
+      # Now add the new node whever prev ended up
       if givenVal < prevNode.val
         prevNode.leftNode = TreeNode.new(givenVal)
       else
         prevNode.rightNode = TreeNode.new(givenVal)
       end
     end
-    @size += 1
   end
 
-  def clear
+  def clear #for clearing out a tree by cutting the root
     @root = nil
-    @size = 0
   end
 
-  def _map(rootNode, givenFunction, isPreOrder, isPrinting)
+  def _map(rootNode, givenFunction, isPreOrder, isPrinting) #Applies given function to all members of tree
     return if rootNode.nil?
     moddedVal = nil
-    unless isPreOrder
+    unless isPreOrder #goes from left to right
       _map(rootNode.leftNode, givenFunction, isPreOrder, isPrinting)
       moddedVal = givenFunction.call(rootNode.val)
       puts "#{moddedVal}" if isPrinting
@@ -55,17 +49,17 @@ class BST_Set
     end
   end
 
-  def map(givenFunction, isPreOrder: false, isPrinting: false)
+  def map(givenFunction, isPreOrder: false, isPrinting: false) #helper function to abstract away passing in root
     _map(@root, givenFunction, isPreOrder, isPrinting)
   end
 
-  def to_s
+  def to_s #override of to string, returns  values inside parenthesis & comma separated
     s = ""
     map(->(val) { s += "#{val}, " })
     "(#{s[0...-2]})"
   end
 
-  def contains?(givenVal) #Returns value in ~O(logN) time
+  def contains?(givenVal) #Checks if value is present in ~O(logN) time
     traveler = @root
     until traveler.nil?
       if traveler.val == givenVal
@@ -79,10 +73,10 @@ class BST_Set
     return false #We could have simply mapped but that would be inefficient O(N) time
   end
 
-  class TreeNode
-    attr_accessor :leftNode, :rightNode, :val
+  class TreeNode #Subclass for our nodes
+    attr_accessor :leftNode, :rightNode, :val #getters and setters
 
-    def initialize(givenVal)
+    def initialize(givenVal) #When new node is made sets the value and sets leafs to nil
       @leftNode = nil
       @rightNode = nil
       @val = givenVal
